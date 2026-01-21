3e2e3c214bdbff6de3cd5436888125c4516cd174
tejasvi12th <66620382+tejasvi12th@users.noreply.github.com>
2026-01-22 00:18:07 +0530

    Start WaterService in MainActivity onCreate

--- app/src/main/java/com/example/watertracker/MainActivity.kt
+++ app/src/main/java/com/example/watertracker/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.watertracker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -9,6 +10,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startService(Intent(this, WaterService::class.java))
 
         val text = findViewById<TextView>(R.id.textView)
         text.text = "Water Tracker is running 💧"
